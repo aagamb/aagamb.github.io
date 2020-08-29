c7a3f3264385df7fb472c2dcf32dacc0e0b57c01
aagamb <69706585+aagamb@users.noreply.github.com>
2020-08-29 21:44:48 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,17 @@
+// function on_submit(){
+//   var x = document.getElementById('after_submit');
+//   if (x.style.visibility === 'hidden') {
+//     x.style.visibility = 'visible';
+//   } else {
+//     x.style.visibility = 'hidden';
+//   }
+// }
+
+function on_submit(){
+  alert('yo bois');
+}
+
+function  photo_gallery_on(){
+  document.getElementsByClassName('photo_gallery_button').style.backgroundImage = url('Assignment 3.png')
+  console.log('helllo')
+}
